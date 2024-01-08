4b3e52794a9df7a14029bd8e31cb954f0f5210d2
ANAND <sajithanand99@gmail.com>
2024-01-08 22:47:55 +0530

    first commit

---
+++ MeCart/orders/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from customers.models import Customer
+from product.models import Product
+# Create your models here.
+
+class Order(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    cart_stage=0
+    order_confirmed=1
+    order_processed=2
+    order_delivered=3
+    order_rejected=4
+    status_choice=((order_processed,"Order_processed"),
+                   (order_delivered,"Order_delivered"),
+                   (order_rejected,"Order_rejected"))
+    
+    order_status=models.IntegerField(choices=status_choice,default=cart_stage)
+    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,related_name='orders')
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    
+class OrderedtItem(models.Model):
+    product=models.ForeignKey(Product,related_name='added_carts',on_delete=models.SET_NULL,null=True)
+    quantity=models.IntegerField(default=1)
+    owner=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='added_items')
+    
+    
